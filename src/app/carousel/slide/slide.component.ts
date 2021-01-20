93ea0df4b651241eef22cd102ff819ef6926fb81
vipin55 <49005230+vipin55@users.noreply.github.com>
2021-01-20 21:13:38 +0530

    Add files via upload

---
+++ src/app/carousel/slide/slide.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-slide',
+  templateUrl: './slide.component.html',
+  styleUrls: ['./slide.component.scss']
+})
+export class SlideComponent implements OnInit {
+
+  constructor() { }
+
+  @Input() slide;
+
+  ngOnInit(): void {
+  }
+
+}
